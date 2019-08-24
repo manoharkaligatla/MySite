c6332ec2b090f239d1966fcea799cbb2f2abc0e9
Manohar <manoharkaligatla@gmail.com>
2019-08-24 14:49:04 +0530

    Updates

---
+++ precache-manifest.78894a6891ce35ef3c5ee6b20b3d0c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8a30d9b291611e21bec4aba3f2a939",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "23f3ae3649ae8568e9e6",
+    "url": "/mysite/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4d962988771386c85009",
+    "url": "/mysite/static/js/2.5d343547.chunk.js"
+  },
+  {
+    "revision": "23f3ae3649ae8568e9e6",
+    "url": "/mysite/static/js/main.d2fd6f27.chunk.js"
+  },
+  {
+    "revision": "bbda72e4fea4fcb3a10b",
+    "url": "/mysite/static/js/runtime~main.db31af97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mysite/static/media/logo.5d5d9eef.svg"
+  }
+]);
