6e4f6b5cf412ea495481ea1fb711ea4dff66ef88
Manohar <manoharkaligatla@gmail.com>
2019-08-27 23:21:12 +0530

    build change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacjhgjght
         </a>
       </header>
     </div>
